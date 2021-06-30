5fdd4b8c9a9345aba0c245ac058e2d28e37b3977
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-06-29 23:47:40 -0300

    Recuperando váriavel de ambiente

--- springbootconfig/src/main/java/com/github/gabrielpadilh4/springbootconfig/AppController.java
+++ springbootconfig/src/main/java/com/github/gabrielpadilh4/springbootconfig/AppController.java
@@ -10,8 +10,16 @@ public class AppController {
     @Value("${app.message}")
     public String appMessage;
 
+    @Value("${JAVA_HOME:NOT_DEFINED}") // system variable
+    private String javaHomeVariable;
+
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getJavaHomeVariable() {
+        return "%JAVA_HOME% -> " + javaHomeVariable;
     }
 }
