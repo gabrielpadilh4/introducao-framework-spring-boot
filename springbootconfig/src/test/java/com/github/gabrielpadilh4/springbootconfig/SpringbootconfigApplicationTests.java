dcf37305999bd151d18613e236c992c3f23a215f
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-06-29 23:27:18 -0300

    Utilizando os arquivos .properties para diferentes ambientes

---
+++ springbootconfig/src/test/java/com/github/gabrielpadilh4/springbootconfig/SpringbootconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielpadilh4.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
